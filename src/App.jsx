071facad68fd475c3b251fd521dfff6c264b6b39
veerbal1 <veerbal1singh1@gmail.com>
2022-12-25 13:07:13 +0530

    Added list at main App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { useState } from 'react';
 import './App.css';
+import TaskList from './components/tasks/TaskList';
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <h1>Tasks</h1>
+      <TaskList />
+    </div>
+  );
 }
 
 export default App;
