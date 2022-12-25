2369fd2bfa554908f2f1e3f89ffae66e6840e483
veerbal1 <veerbal1singh1@gmail.com>
2022-12-25 13:30:08 +0530

    Added basic functions to useTasks hook

--- src/hooks/useTasks.js
+++ src/hooks/useTasks.js
@@ -1,29 +1,32 @@
 import React from 'react';
 
 const useTasks = () => {
-  const [tasks, setTasks] = React.useState([
-    {
-      id: 1,
-      name: 'Task 1',
-      description: 'Description 1',
-      status: 'Done',
-    },
-    {
-      id: 2,
-      name: 'Task 2',
-      description: 'Description 2',
-      status: 'Done',
-    },
-    {
-      id: 3,
-      name: 'Task 3',
-      description: 'Description 3',
-      status: 'Pending',
-    },
-  ]);
-  
+  const [tasks, setTasks] = React.useState([]);
+
+  const createTask = (task) => {
+    setTasks([...tasks, task]);
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const updateTask = (id, updatedTask) => {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === id) {
+          return updatedTask;
+        }
+        return task;
+      })
+    );
+  };
+
   return {
     tasks,
+    createTask,
+    updateTask,
+    deleteTask,
   };
 };
 
