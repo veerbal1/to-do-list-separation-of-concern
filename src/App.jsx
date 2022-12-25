e04e9a4ac9304e597aa9e9b9600f9a5d4a4355bc
veerbal1 <veerbal1singh1@gmail.com>
2022-12-25 13:39:39 +0530

    Added Create task component at main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from 'react';
+import { Grid } from '@mui/material';
 import './App.css';
+import CreateTask from './components/tasks/CreateTask';
 import TaskList from './components/tasks/TaskList';
+import H3 from './components/ui/typography/h3';
 
 function App() {
   return (
     <div className="App">
-      <h1>Tasks</h1>
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          padding: 2,
+        }}
+        justifyContent="center"
+      >
+        <Grid item xs={12}>
+          <H3 title="Task Manager" align="center" />
+        </Grid>
+        <CreateTask />
+      </Grid>
       <TaskList />
     </div>
   );
