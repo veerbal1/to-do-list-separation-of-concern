8e9b1221a001873dad77d98aea601d749e12f533
veerbal1 <veerbal1singh1@gmail.com>
2022-12-25 13:29:43 +0530

    Updated input field

---
+++ src/components/ui/input/index.jsx
@@ -0,0 +1,8 @@
+import { TextField } from '@mui/material';
+import React from 'react';
+
+const Input = (props) => {
+  return <TextField fullWidth size="small" variant="outlined" {...props} />;
+};
+
+export default Input;
