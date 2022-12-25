5801c4c7407dd6b2ad08ba70efbdc9716735bac1
veerbal1 <veerbal1singh1@gmail.com>
2022-12-25 13:54:46 +0530

    Added value handling functionality in CreateTask component

--- src/components/tasks/CreateTask.jsx
+++ src/components/tasks/CreateTask.jsx
@@ -1,20 +1,61 @@
 import { Grid, Stack } from '@mui/material';
-import React from 'react';
+import React, { useCallback, useId } from 'react';
+import useTasks from '../../hooks/useTasks';
 import Button from '../ui/buttons';
 import Input from '../ui/input';
 
 const CreateTask = () => {
+  const id = useId();
+
+  const { createTask } = useTasks();
+  const [task, setTask] = React.useState({
+    title: '',
+    description: '',
+    status: 'pending',
+  });
+
+  const handleCreateTask = useCallback(() => {
+    console.log('create task');
+    // If title is empty, do not create task
+    if (!task.title) {
+      return;
+    }
+    createTask({ task, id });
+    setTask({
+      title: '',
+      description: '',
+    });
+  }, []);
+
+  const handleInputChange = useCallback((e) => {
+    const { name, value } = e.target;
+    setTask((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }, []);
+
   return (
     <Grid item xs={12}>
       <Stack direction="column" spacing={2}>
         <div>
           <Stack direction="row" spacing={2}>
-            <Input label="New Task" />
-            <Input label="Description" />
+            <Input
+              name="title"
+              label="New Task"
+              value={task.title}
+              onChange={handleInputChange}
+            />
+            <Input
+              name="description"
+              label="Description"
+              value={task.description}
+              onChange={handleInputChange}
+            />
           </Stack>
         </div>
         <div>
-          <Button title="Create Task" fullWidth/>
+          <Button title="Create Task" fullWidth onClick={handleCreateTask} />
         </div>
       </Stack>
     </Grid>
